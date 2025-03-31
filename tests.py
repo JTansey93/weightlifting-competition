286f340679db1b633cef36cd861a509f776f9cd7
James <57265750+JTansey93@users.noreply.github.com>
2025-03-31 21:47:54 +0100

    More Tests and Comp Logic
    Added some more tests for Sinclair scores and some tests and logic around how a competition works in terms of who the next lifter up is

--- tests.py
+++ tests.py
@@ -2,14 +2,29 @@ import unittest
 from weightlifting_competition.competition import Competition
 from weightlifting_competition.competitor import Competitor
 
-class TestCompetitor(unittest.TestCase):
+karlos = Competitor("Karlos Nasar", "M", "2004-05-12", 89, 180, 225)
+karlos.snatch(1, 180, True)
+karlos.cnj(1, 225, True)
+lasha = Competitor("Lasha Talkhadze", "M", "1993-10-03", 193.3, 180, 225)
+lasha.snatch(1, 225, True)
+lasha.cnj(1, 267, True)
+competition = Competition([karlos, lasha])
+
+class Tester(unittest.TestCase):
 
     def test_total(self):
-        karlos = Competitor("Karlos Nasar", "M", "2004-05-12", 89)
-        karlos.snatch(1, 170, True)
-        karlos.cnj(1, 210, True)
-        self.assertEqual(karlos.get_total(), 380)
-        self.assertEqual(karlos.sinclair(), 459.33)
+        self.assertEqual(karlos.get_total(), 405)
+    
+    def test_sinclair(self):
+        self.assertEqual(karlos.sinclair(), 489.55)
+        self.assertEqual(lasha.sinclair(), 492)
+
+    def test_next_lifter(self):
+        self.assertLess(karlos.next_snatch, lasha.next_snatch)
+        self.assertEqual(competition.next_lifter(), karlos)
+
+    def test_winner(self):
+        self.assertEqual(competition.winner(), lasha)
 
 if __name__ == '__main__':
     unittest.main()
--- weightlifting_competition/competitor.py
+++ weightlifting_competition/competitor.py
@@ -5,11 +5,13 @@ F_WEIGHT_CLASSES = [48, 53, 58, 63, 69, 77, 86]
 
 class Competitor():
 
-    def __init__(self, name, gender, dob, bodyweight):
+    def __init__(self, name, gender, dob, bodyweight, first_snatch, first_cnj):
         self.name = name
         self.dob = dob
         self.gender = gender
         self.bodyweight = bodyweight
+        self.next_snatch = first_snatch
+        self.next_cnj = first_cnj
         self.snatch_attempts = [[0, False], [0, False], [0, False]]
         self.cnj_attempts = [[0, False], [0, False], [0, False]]
         self.best_snatch = 0
@@ -31,11 +33,15 @@ class Competitor():
         self.snatch_attempts[attempt_no] = [weight, make]
         if make:
             self.best_snatch = weight
+            if attempt_no < 3:
+                self.next_snatch = weight + 1
 
     def cnj(self, attempt_no, weight, make):
         self.cnj_attempts[attempt_no] = [weight, make]
         if make:
             self.best_cnj = weight
+            if attempt_no < 3:
+                self.next_snatch = weight + 1
 
     def get_total(self):
         return self.best_snatch + self.best_cnj
